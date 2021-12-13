d804b65013edf30c57433446a5286c03dca4c94b
mypar <mypar@LAPTOP-OB3DI36Q>
2021-12-13 18:17:56 +0900

    버전 : 2.0 변경내용 : 로그인 기능 업그레이드 작성자 : 박민이 날짜 : 2021/12/13

--- Test826/src/TestJava.java
+++ Test826/src/TestJava.java
@@ -1,6 +1,7 @@
 public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT World!");
-		System.out.println("Version : 1.0");
+		System.out.println("Version : 2.0");
+		System.out.println("·Î±×ÀÎ µÊ!!");
 	}
 }
